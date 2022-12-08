ee3d1fa85c912f104d8471fb5478685f39aabbae
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2022-12-08 12:17:23 -0500

    Change title

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -18,7 +18,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Roast My Looker Instance",
   viewport: "width=device-width,initial-scale=1",
 });
 
